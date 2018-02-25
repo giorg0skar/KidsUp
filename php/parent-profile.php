9dc81febf80246ea057517dc10c08172697823a0
JimmyA1994 <dimitrisadamis1994@gmail.com>
2018-02-25 07:54:18 +0000

    Update parent-profile.php to add favicon + change tab name to greek.

--- php/parent-profile.php
+++ php/parent-profile.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Profile</title>
+    <title>Προφίλ Γονέα</title>
+    <link rel="shortcut icon" type="image" href="../assets/img/favicon.ico"/>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
