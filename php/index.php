19296d8416d101608f34d718649a792d06d7cddd
Nikos Dragazis <ndragazis@outlook.com.gr>
2018-02-25 17:35:14 +0200

    Fixed href in cards.

--- php/index.php
+++ php/index.php
@@ -152,7 +152,7 @@
 ?>
           <div class="col-lg-4 col-md-6 mb-4">
             <div class="card h-100">
-              <a href="buyTicket.php"><img class="card-img-top" src= <?php echo $row['pictureURL']; ?> alt="" ></a>
+              <a href="#" onclick="post_actid(<?php echo $row['ActID'] ?>);return false;"><img class="card-img-top" src= <?php echo $row['pictureURL']; ?> alt="" ></a>
               <div class="card-body">
                 <h4 class="card-title">
                   <a href="#" onclick="post_actid(<?php echo $row['ActID'] ?>);return false;">
